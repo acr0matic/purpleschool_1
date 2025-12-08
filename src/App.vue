30ef880c8b847a960facd3d02296bf54d4c1341e
acr0matic <work.acr0matic@gmail.com>
2025-12-08 23:56:16 +0300

    feat: кнопка

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <p>Текущая дата: {{ getCurrentDate() }}</p>
+    <Button></Button>
   </div>
 </template>
 
 <script setup>
+import Button from "./components/Button.vue";
+
 const dateFormatter = new Intl.DateTimeFormat('ru-RU', {
   day: '2-digit',
   month: '2-digit',
---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="button">Кнопка</button>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/abstracts/vars';
+
+.button {
+  min-width: 140px;
+  border-radius: 10px;
+  border: none;
+  outline: none;
+  padding: 10px 24px;
+
+  background-size: 100%;
+  background-image: var(--gradient);
+  color: var(--text-primary);
+
+  font-weight: vars.$weight-SemiBold;
+
+  cursor: pointer;
+  transition: 0.3s ease;
+
+  @media (hover: hover) {
+    &:hover {
+      background-size: 200%;
+      background-position: right;
+    }
+  }
+}
+</style>
