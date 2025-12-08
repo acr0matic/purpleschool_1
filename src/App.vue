8fe98c23c959ea79f79522be7d5ab487c86095f0
acr0matic <work.acr0matic@gmail.com>
2025-12-08 21:14:03 +0300

    feat: вывод даты

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-   
+    <p>Текущая дата: {{ getCurrentDate() }}</p>
   </div>
 </template>
 
 <script setup>
+const dateFormatter = new Intl.DateTimeFormat('ru-RU', {
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric',
+});
 
+const getCurrentDate = (currentDate = new Date()) => {
+  return dateFormatter.format(currentDate);
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
